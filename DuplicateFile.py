8a7dc2a02cab4820b838d939a290c33d67f917ca
Loamhoof <soeren@medard.xyz>
2015-04-18 16:27:02 +0200

    Duplicatefile open created file

--- DuplicateFile.py
+++ DuplicateFile.py
@@ -1,13 +1,17 @@
-import sublime_plugin
+import sublime, sublime_plugin
 import os, time, shutil
 
 
 class DuplicateFile(sublime_plugin.ApplicationCommand):
-    def run(self, paths):
+    @staticmethod
+    def run(paths):
         file_ = paths[0]
         file_name, file_ext = os.path.splitext(file_)
         copy = "%s_%d%s" % (file_name, int(time.time()), file_ext)
         shutil.copy2(file_, copy)
+        print(os.path.basename(copy))
+        sublime.active_window().open_file(os.path.basename(copy))
 
-    def is_visible(self, paths):
+    @staticmethod
+    def is_visible(paths):
         return os.path.isfile(paths[0])
